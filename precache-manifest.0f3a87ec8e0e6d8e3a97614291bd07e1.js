ff39c09474ba3a0cc3a7e4ed967c61613ef6e37e
Javier Minchala <javierminchala@gmail.com>
2019-09-30 16:51:38 +0900

    Updates

---
+++ precache-manifest.0f3a87ec8e0e6d8e3a97614291bd07e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6a21c2c5ee7414634eb1495a0c1b235",
+    "url": "/MHP3rdStringTableEditor/index.html"
+  },
+  {
+    "revision": "4bcc5bed72e27f468815",
+    "url": "/MHP3rdStringTableEditor/static/css/main.9e3b0042.chunk.css"
+  },
+  {
+    "revision": "7b5b2eedb2ca16c4e6c3",
+    "url": "/MHP3rdStringTableEditor/static/js/2.9f8e0f17.chunk.js"
+  },
+  {
+    "revision": "4bcc5bed72e27f468815",
+    "url": "/MHP3rdStringTableEditor/static/js/main.ff9f6739.chunk.js"
+  },
+  {
+    "revision": "4e198e00ea2f6a7a853d",
+    "url": "/MHP3rdStringTableEditor/static/js/runtime-main.a8dfd265.js"
+  }
+]);
